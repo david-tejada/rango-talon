f0c347541dc6946c79ac3c27eac05e5ebae444dd
David Tejada <davidxtejada@gmail.com>
2022-06-14 12:17:52 +0200

    Improve error handling

--- browser.py
+++ browser.py
@@ -97,15 +97,20 @@ def read_json_response_with_timeout() -> Any:
     timeout_time = time.perf_counter() + RANGO_COMMAND_TIMEOUT_SECONDS
     sleep_time = MINIMUM_SLEEP_TIME_SECONDS
     message = None
+    initial_raw_text = clip.text()
     while True:
         raw_text = clip.text()
         try:
             message = json.loads(raw_text)
         # We make sure the message is valid JSON. For example, if a click command
         # results in something being copied to the clipboard and we check the clipboard
-        # before Rango has time to copy the response to the clipboard
-        except ValueError:
-            pass
+        # before Rango has time to copy the response to the clipboard.
+        except ValueError as error:
+            if initial_raw_text != raw_text:
+                continue
+            else:
+                # Sanity check to make sure the initial request was valid JSON
+                raise ValueError("The request message wasn't valid JSON")
 
         if message["type"] == "response":
             break
@@ -185,10 +190,7 @@ class BrowserActions:
         with clip.revert():
             clip.set_text(json_message)
             actions.key("ctrl-shift-insert")
-            try:
-                response = read_json_response_with_timeout()
-            except Exception:
-                return None
+            response = read_json_response_with_timeout()
 
         if response["action"]["type"] == "noAction":
             return None
