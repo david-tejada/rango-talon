53eee0aa6a0a118b1c3f2f23b7a5d4607f70adc2
David Tejada <davidxtejada@gmail.com>
2022-12-31 11:50:39 +0100

    Reimplement go_back and go_forward for Chromium browsers

---
+++ rango_chromium.py
@@ -0,0 +1,19 @@
+from talon import Context, actions, app
+
+ctx = Context()
+ctx.matches = r"""
+tag: browser
+app: chrome
+app: brave
+app: vivaldi
+app: microsoft_edge
+app: opera
+"""
+
+@ctx.action_class("browser")
+class BrowserActions:
+    def go_back():
+        actions.user.rango_command_without_target("historyGoBack")
+
+    def go_forward():
+        actions.user.rango_command_without_target("historyGoForward")
