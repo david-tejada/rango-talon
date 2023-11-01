27980ab80e4e659c7e1a80817cd9cf7ccd710b0c
David Tejada <davidxtejada@gmail.com>
2023-11-01 18:11:49 +0100

    Refactor and move default implementations to mod

---
+++ src/command_server.py
@@ -0,0 +1,146 @@
+from talon import Module, clip, actions
+import json
+import time
+from typing import Union, Any
+
+mod = Module()
+
+MINIMUM_SLEEP_TIME_SECONDS = 0.0005
+
+
+# This doesn't wait for the extension to respond to avoid creating an infinite
+# loop if the extension is not responsive
+def send_request_timed_out():
+    message = {"version": 1, "type": "request", "action": {"type": "requestTimedOut"}}
+    json_message = json.dumps(message)
+    clip.set_text(json_message)
+    actions.user.rango_type_hotkey()
+    actions.sleep("200ms")
+
+
+def read_json_response_with_timeout(timeout_seconds) -> Any:
+    """Repeatedly tries to read a json object from the clipboard, waiting
+    until the message type is "response"
+
+    Raises:
+        Exception: If we timeout waiting for a response
+
+    Returns:
+        Any: The json-decoded contents of the file
+    """
+    timeout_time = time.perf_counter() + timeout_seconds
+    sleep_time = MINIMUM_SLEEP_TIME_SECONDS
+    message = None
+    initial_raw_text = clip.text()
+    while True:
+        raw_text = clip.text()
+        try:
+            message = json.loads(raw_text)
+        # We make sure the message is valid JSON. For example, if a click command
+        # results in something being copied to the clipboard and we check the clipboard
+        # before Rango has time to copy the response to the clipboard.
+        except ValueError as error:
+            if initial_raw_text != raw_text:
+                continue
+            else:
+                # Sanity check to make sure the initial request was valid JSON
+                raise ValueError("The request message wasn't valid JSON")
+
+        if message["type"] == "response":
+            break
+
+        actions.sleep(sleep_time)
+
+        time_left = timeout_time - time.perf_counter()
+
+        if time_left < 0:
+            send_request_timed_out()
+            raise Exception("Timed out waiting for response")
+
+        # NB: We use minimum sleep time here to ensure that we don't spin with
+        # small sleeps due to clock slip
+        sleep_time = max(min(sleep_time * 2, time_left), MINIMUM_SLEEP_TIME_SECONDS)
+
+    return message
+
+
+def send_request_and_wait(action: dict) -> Any:
+    message = {"version": 1, "type": "request", "action": action}
+    json_message = json.dumps(message)
+    with clip.revert():
+        clip.set_text(json_message)
+        actions.user.rango_type_hotkey()
+        response = read_json_response_with_timeout(3.0)
+
+    return response
+
+
+def handle_response(response: Any, request_action: dict):
+    response_actions = response.get("actions")
+    if actions == None:
+        actions.app.notify(
+            "Rango-talon is ahead of the Rango extension. Restart the browser to update the Rango extension"
+        )
+
+        return
+
+    result = None
+
+    for action in response_actions:
+        name = action["name"]
+
+        if name == "focusPageAndResend":
+            try:
+                actions.browser.focus_page()
+            except NotImplementedError:
+                actions.browser.focus_address()
+                actions.key("esc:3")
+
+            response = send_request_and_wait(request_action)
+            result = handle_response(response, request_action)
+
+        if name == "copyToClipboard":
+            actions.clip.set_text(action["textToCopy"])
+
+        if name == "typeTargetCharacters":
+            actions.insert(action["target"][0])
+
+        if name == "focusPage":
+            try:
+                actions.browser.focus_page()
+            except NotImplementedError:
+                actions.browser.focus_address()
+                actions.key("esc:3")
+
+        if name == "key":
+            actions.key(action["key"])
+
+        if name == "editDelete":
+            actions.edit.delete()
+
+        if name == "sleep":
+            if "ms" in action:
+                actions.sleep(f"{action['ms']}ms")
+            else:
+                actions.sleep("200ms")
+
+        if name == "responseValue":
+            result = action["value"]
+
+        if name == "openInNewTab":
+            actions.app.tab_open()
+            actions.browser.go(action["url"])
+
+    return result
+
+
+@mod.action_class
+class Actions:
+    def rango_type_hotkey():
+        """Presses the rango hotkey to read the command from the clipboard"""
+        actions.key("ctrl-shift-insert")
+
+    def rango_run_command(action: dict):
+        """Sends a request to the Rango extension, waits for the response and handles it"""
+        response = send_request_and_wait(action)
+        return handle_response(response, action)
--- src/rango.py
+++ src/rango.py
@@ -1,7 +1,5 @@
-from talon import Module, Context, actions, clip, settings, app
-import json
-import time
-from typing import Any, Union
+from talon import Module, Context, actions
+from typing import Union
 
 mod = Module()
 ctx = Context()
@@ -57,203 +55,14 @@ def rango_tab_marker(m) -> str:
     return "".join(m)
 
 
-MINIMUM_SLEEP_TIME_SECONDS = 0.0005
-
-
-# This doesn't wait for the extension to respond to avoid creating an infinite
-# loop if the extension is not responsive
-def send_request_timed_out():
-    message = {"version": 1, "type": "request", "action": {"type": "requestTimedOut"}}
-    json_message = json.dumps(message)
-    clip.set_text(json_message)
-    actions.user.rango_type_hotkey()
-    actions.sleep("200ms")
-
-
-def read_json_response_with_timeout(timeout_seconds) -> Any:
-    """Repeatedly tries to read a json object from the clipboard, waiting
-    until the message type is "response"
-
-    Raises:
-        Exception: If we timeout waiting for a response
-
-    Returns:
-        Any: The json-decoded contents of the file
-    """
-    timeout_time = time.perf_counter() + timeout_seconds
-    sleep_time = MINIMUM_SLEEP_TIME_SECONDS
-    message = None
-    initial_raw_text = clip.text()
-    while True:
-        raw_text = clip.text()
-        try:
-            message = json.loads(raw_text)
-        # We make sure the message is valid JSON. For example, if a click command
-        # results in something being copied to the clipboard and we check the clipboard
-        # before Rango has time to copy the response to the clipboard.
-        except ValueError as error:
-            if initial_raw_text != raw_text:
-                continue
-            else:
-                # Sanity check to make sure the initial request was valid JSON
-                raise ValueError("The request message wasn't valid JSON")
-
-        if message["type"] == "response":
-            break
-
-        actions.sleep(sleep_time)
-
-        time_left = timeout_time - time.perf_counter()
-
-        if time_left < 0:
-            send_request_timed_out()
-            raise Exception("Timed out waiting for response")
-
-        # NB: We use minimum sleep time here to ensure that we don't spin with
-        # small sleeps due to clock slip
-        sleep_time = max(min(sleep_time * 2, time_left), MINIMUM_SLEEP_TIME_SECONDS)
-
-    return message
-
-
-def send_request_and_wait_for_response(action: dict, timeout_seconds: float = 3.0):
-    message = {"version": 1, "type": "request", "action": action}
-    json_message = json.dumps(message)
-    with clip.revert():
-        clip.set_text(json_message)
-        actions.user.rango_type_hotkey()
-        response = read_json_response_with_timeout(timeout_seconds)
-
-    response_actions = response.get("actions")
-
-    if response_actions == None:
-        actions.app.notify(
-            "Rango-talon is ahead of the Rango extension. Restart the browser to update the Rango extension"
-        )
-        return
-
-    result = None
-
-    for response_action in response_actions:
-        name = response_action["name"]
-
-        if name == "focusPageAndResend":
-            try:
-                actions.browser.focus_page()
-            except NotImplementedError:
-                actions.browser.focus_address()
-                actions.key("esc:3")
-
-            send_request_and_wait_for_response(action, timeout_seconds)
-
-        if name == "copyToClipboard":
-            actions.clip.set_text(response_action["textToCopy"])
-
-        if name == "typeTargetCharacters":
-            actions.insert(action["target"][0])
-
-        if name == "focusPage":
-            try:
-                actions.browser.focus_page()
-            except NotImplementedError:
-                actions.browser.focus_address()
-                actions.key("esc:3")
-
-        if name == "key":
-            actions.key(response_action["key"])
-
-        if name == "editDelete":
-            actions.edit.delete()
-
-        if name == "sleep":
-            if "ms" in response_action:
-                actions.sleep(f"{response_action['ms']}ms")
-            else:
-                actions.sleep("200ms")
-
-        if name == "responseValue":
-            result = response_action["value"]
-
-        if name == "openInNewTab":
-            actions.app.tab_open()
-            actions.browser.go(response_action["url"])
-
-    return result
-
-
 @mod.action_class
 class Actions:
-    def rango_type_hotkey():
-        """Presses the rango hotkey to read the command from the clipboard"""
-
-    def rango_command_with_target(
-        actionType: str,
-        target: Union[str, list[str]],
-        arg: Union[str, float, None] = None,
-    ):
-        """Executes a Rango command"""
-
-    def rango_command_without_target(
-        actionType: str,
-        arg: Union[str, float, None] = None,
-        arg2: Union[str, None] = None,
-    ):
-        """Executes a Rango command without a target"""
-
     def rango_toggle_hints():
         """It toggles the Rango hints globally on or off"""
+        actions.user.rango_command_without_target("toggleHints")
 
     def rango_try_to_focus_and_check_is_editable(target: Union[str, list[str]]):
         """Tries to focus an element marked with a hint (clicking if it's not a link) and returns true if the active element is editable"""
-
-    def rango_insert_text_to_input(
-        text: str, target: Union[str, list[str]], pressEnter: bool
-    ):
-        """Inserts a given text to an input marked with the target hint"""
-
-    def rango_clear_input(target: Union[str, list[str]]):
-        """Removes the contents of an input"""
-
-    def rango_enable_direct_clicking():
-        """Enables rango direct mode so that the user doesn't have to say 'click' before the hint letters"""
-
-    def rango_disable_direct_clicking():
-        """Disables rango direct mode"""
-
-    def rango_run_action_on_reference(command: str, reference: str):
-        """Runs a Rango command on a mark"""
-
-
-@ctx.action_class("user")
-class UserActions:
-    def rango_type_hotkey():
-        actions.key("ctrl-shift-insert")
-
-    def rango_command_with_target(
-        actionType: str,
-        target: Union[str, list[str]],
-        arg: Union[str, float, None] = None,
-    ):
-        if isinstance(target, str):
-            target = [target]
-        action = {"type": actionType, "target": target}
-        if arg:
-            action["arg"] = arg
-        return send_request_and_wait_for_response(action)
-
-    def rango_command_without_target(
-        actionType: str,
-        arg: Union[str, float, None] = None,
-        arg2: Union[str, None] = None,
-    ):
-        action = {"type": actionType}
-        if arg:
-            action["arg"] = arg
-        if arg2:
-            action["arg2"] = arg2
-        return send_request_and_wait_for_response(action)
-
-    def rango_try_to_focus_and_check_is_editable(target: Union[str, list[str]]):
         return actions.user.rango_command_with_target(
             "tryToFocusElementAndCheckIsEditable", target
         )
@@ -261,6 +70,7 @@ class UserActions:
     def rango_insert_text_to_input(
         text: str, target: Union[str, list[str]], pressEnter: bool
     ):
+        """Inserts a given text to an input marked with the target hint"""
         if actions.user.rango_try_to_focus_and_check_is_editable(target):
             actions.edit.select_all()
             actions.edit.delete()
@@ -272,15 +82,13 @@ class UserActions:
                 actions.key("enter")
 
     def rango_clear_input(target: Union[str, list[str]]):
+        """Removes the contents of an input"""
         if actions.user.rango_try_to_focus_and_check_is_editable(target):
             actions.edit.select_all()
             actions.edit.delete()
 
-    # Necessary for talon_hud
-    def rango_toggle_hints():
-        actions.user.rango_command_without_target("toggleHints")
-
     def rango_run_action_on_reference(command: str, reference: str):
+        """Runs a Rango command on a mark"""
         actions.user.rango_command_without_target(
             "runActionOnReference", command, reference
         )
