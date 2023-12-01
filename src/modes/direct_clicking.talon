71884bda98f3f2877cef9f838159b1cb385d6818
David Tejada <davidxtejada@gmail.com>
2023-12-01 12:48:31 +0100

    Create tag for forcing explicit clicking

--- src/modes/direct_clicking.talon
+++ src/modes/direct_clicking.talon
@@ -1,5 +1,6 @@
 tag: browser
 and tag: user.rango_direct_clicking
+and not tag: user.rango_explicit_clicking
 -
 
 ^<user.rango_target>$: user.rango_command_with_target("directClickElement", rango_target)
--- src/tags.py
+++ src/tags.py
@@ -7,6 +7,10 @@ mod.tag(
     desc="Tag for enabling direct clicking in Rango",
 )
 mod.tag(
+    "rango_explicit_clicking",
+    desc="Tag for forcing explicit clicking in Rango. This tag wins over rango_direct_clicking if both are enabled",
+)
+mod.tag(
     "rango_exclude_singles",
     desc="Tag for excluding hints that contain only one letter",
 )
